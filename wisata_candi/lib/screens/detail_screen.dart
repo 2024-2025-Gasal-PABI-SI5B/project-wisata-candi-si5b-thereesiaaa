1eabfa67ed9f7b0bac3d0807e6e5ce45fd4c167f
thereesiaaa <theresiachintya27@gmail.com>
2024-11-07 09:21:13 +0700

    detail header

--- wisata_candi/lib/screens/detail_screen.dart
+++ wisata_candi/lib/screens/detail_screen.dart
@@ -1,13 +1,59 @@
 import 'package:flutter/material.dart';
+import 'package:wisata_candi/models/candi.dart';
 
 class DetailScreen extends StatelessWidget {
-  const DetailScreen({super.key});
+  final Candi candi;
+
+  const DetailScreen({super.key, required this.candi});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
-        children: [],
+        children: [
+          //detail utama
+          //image utama
+          Stack(
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8),
+                child: ClipRRect(borderRadius: BorderRadius.circular(20),
+                child: Image.asset(
+                  candi.imageAsset,
+                  width: double.infinity,
+                  height: 300,
+                  fit: BoxFit.cover,
+                ),
+                ),
+                ),
+                //tombol back
+                Padding(
+                  padding: const EdgeInsets.all(16),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.deepPurpleAccent[100]?.withOpacity(0.8),
+                      shape: BoxShape.circle,
+                    ),
+                    child: IconButton(
+                      onPressed: (){},
+                      icon: const Icon(Icons.arrow_back)),
+                  ),
+                ),
+            ],
+          ),
+          //detail info
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 16),
+            child: Column(
+              children: [
+                //info atas
+                //info tengah
+                //info bawah
+              ],
+            ),
+          )
+
+        ],
       ),
     );
   }
