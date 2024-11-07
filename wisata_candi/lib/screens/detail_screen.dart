3adcd4960b79fd091c7766cb5ccf599e302760a3
thereesiaaa <theresiachintya27@gmail.com>
2024-11-07 09:56:30 +0700

    info atas tengah bawah

--- wisata_candi/lib/screens/detail_screen.dart
+++ wisata_candi/lib/screens/detail_screen.dart
@@ -43,11 +43,72 @@ class DetailScreen extends StatelessWidget {
           ),
           //detail info
           Padding(
-            padding: EdgeInsets.symmetric(horizontal: 16),
+            padding: const EdgeInsets.symmetric(horizontal: 16),
             child: Column(
               children: [
                 //info atas
+                const SizedBox(height: 16,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(candi.name,
+                    style: const TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold
+                    ),),
+                    IconButton(onPressed: (){}, icon: const Icon(Icons.favorite_border))
+                  ],
+                ),
                 //info tengah
+                const SizedBox(height: 16,),
+                Row(children: [
+                 const Icon(Icons.place, color: Colors.red,),
+                 const SizedBox(width: 8,),
+                 const SizedBox(
+                    width: 70,
+                    child: Text(
+                      'Lokasi',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  Text(': ${candi.location}')
+                ],),
+                Row(children: [
+                 const Icon(Icons.calendar_month, color: Colors.blue,),
+                 const SizedBox(width: 8,),
+                 const SizedBox(
+                    width: 70,
+                    child: Text(
+                      'Dibangun',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  Text(': ${candi.built}')
+                ],),
+                Row(children: [
+                 const Icon(Icons.house, color: Colors.green,),
+                 const SizedBox(width: 8,),
+                 const SizedBox(
+                    width: 70,
+                    child: Text(
+                      'Tipe',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  Text(': ${candi.type}')
+                ],
+                ),
+                //pemisah
+                SizedBox(
+                  height: 16,
+                  ),
+                Divider(
+                  color: Colors.deepPurple.shade100,
+                  ),
+                const SizedBox(
+                  height: 16,
+                )
                 //info bawah
               ],
             ),
