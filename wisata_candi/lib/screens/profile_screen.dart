02cd2b130e4206fddc5f23fede7fd6b540d90c40
teree <theresiachintya27@gmail.com>
2024-11-11 10:52:53 +0700

    profile

--- wisata_candi/lib/screens/profile_screen.dart
+++ wisata_candi/lib/screens/profile_screen.dart
@@ -9,126 +9,189 @@ class ProfileScreen extends StatefulWidget {
 
 class _ProfileScreenState extends State<ProfileScreen> {
   //TODO : 1. deklarasi variabel yg dibutuhkn
-  bool isSignedIn = true;
+  bool isSignedIn = false;
   String fullName = 'Theresia Chintia';
   String userName = 'thereesiaaa';
   int FavoriteCandiCount = 0;
+
+  //TODO 5: Implementasi fungsi SignIn
+  void SignIn() {
+    setState(() {
+      isSignedIn = !isSignedIn;
+    });
+  }
+
+  //TODO 6: implementasi fungsi SignOut
+  void SignOut() {
+    setState(() {
+      isSignedIn = !isSignedIn;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-   return Scaffold(
-    body: Stack(
-      children: [
-        Container(
-          height: 200,
-          width: double.infinity,
-          color: Colors.deepPurple,
-        ),
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16),
-          child: Column(
-            children: [
-              //TODO : 2. Buat profile header (gbr prfile)
-              Align(
-                alignment: Alignment.topCenter,
-                child: Padding(
-                  padding: const EdgeInsets.only(top: 150),
-                  child: Stack(
-                    alignment: Alignment.bottomRight,
-                    children: [
-                      Container(
-                        decoration: BoxDecoration(
-                          border: Border.all(
-                            color: Colors.deepPurple,
-                            width: 2,
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            height: 200,
+            width: double.infinity,
+            color: Colors.deepPurple,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: Column(
+              children: [
+                //TODO : 2. Buat profile header (gbr prfile)
+                Align(
+                  alignment: Alignment.topCenter,
+                  child: Padding(
+                    padding: const EdgeInsets.only(top: 150),
+                    child: Stack(
+                      alignment: Alignment.bottomRight,
+                      children: [
+                        Container(
+                          decoration: BoxDecoration(
+                              border: Border.all(
+                                color: Colors.deepPurple,
+                                width: 2,
+                              ),
+                              shape: BoxShape.circle),
+                          child: const CircleAvatar(
+                            radius: 50,
+                            backgroundImage:
+                                AssetImage('images/placeholder_image.png'),
                           ),
-                          shape: BoxShape.circle
                         ),
-                        child: const CircleAvatar(
-                          radius: 50,
-                          backgroundImage: AssetImage('images/placeholder_image.png'),
-                        ),
-                      ),
-                      if(isSignedIn)
-                        IconButton(
-                          onPressed: (){},
-                          icon: Icon(
-                            Icons.camera_alt,
-                            color: Colors.deepPurple[50],
-                          ))
-                    ],
+                        if (isSignedIn)
+                          IconButton(
+                              onPressed: () {},
+                              icon: Icon(
+                                Icons.camera_alt,
+                                color: Colors.deepPurple[50],
+                              ))
+                      ],
+                    ),
                   ),
                 ),
-              ),
-              //TODO : 3. bagian profile pp (foto pp)
-              //baris satu profile
-              const SizedBox(height: 20,),
-              Divider(color: Colors.deepPurple[100],),
-              const SizedBox(height: 4,
-              ),
-              Row(
-                children: [
-                  SizedBox(
-                    width: MediaQuery.of(context).size.width/3,
-                    child: const Row(
-                      children: [
-                        Icon(Icons.lock, color: Colors.amber,),
-                        const SizedBox(width: 8,),
-                        Text(
-                          'Pengguna', 
-                          style: TextStyle(
-                            fontSize: 18, fontWeight: FontWeight.bold,
-                          ) ,), 
-                      ],
-                    ),
-                  ),
-                  Expanded(
-                    child: Text(
-                      ': $userName',
-                    style: TextStyle(fontSize: 18),
-                    )),
-              //baris dua profile
-              const SizedBox(height: 20,),
-              Divider(color: Colors.deepPurple[100],),
-              const SizedBox(height: 4,
-              ),
-              Row(
-                children: [
-                  SizedBox(
-                    width: MediaQuery.of(context).size.width/3,
-                    child: const Row(
-                      children: [
-                        Icon(
-                          Icons.lock,
-                          color: Colors.amber,
-                        ),
-                        const SizedBox(
-                          width: 8,
-                        ),
-                        Text(
-                          'asfdhs',
-                          style: TextStyle(
-                            fontSize: 18, fontWeight: FontWeight.bold,
+                //TODO : 3. bagian profile pp (foto pp)
+                //baris satu profile
+                const SizedBox(height: 20),
+                Divider(color: Colors.deepPurple[100]),
+                const SizedBox(height: 4),
+                Row(
+                  children: [
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width / 3,
+                      child: const Row(
+                        children: [
+                          Icon(
+                            Icons.lock,
+                            color: Colors.amber,
                           ),
-                        )
-                      ],
+                          const SizedBox(
+                            width: 8,
+                          ),
+                          Text(
+                            'Pengguna',
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
-                  ),
-                  Expanded(
-                    child: Text(': $userName',
-                    style: TextStyle(fontSize: 18),
-                    ))
-                ],
-              )
-
-                
-                ],
-              ),
-              //TODO : 4. profile action (sign in/out)
-            ],
+                    Expanded(
+                        child: Text(
+                      ': $userName',
+                      style: TextStyle(fontSize: 18),
+                    )),
+                  ],
+                ),
+                //baris dua nama
+                const SizedBox(height: 4),
+                Divider(color: Colors.deepPurple[100]),
+                const SizedBox(height: 4),
+                Row(
+                  children: [
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width / 3,
+                      child: const Row(
+                        children: [
+                          Icon(
+                            Icons.person,
+                            color: Colors.blue,
+                          ),
+                          const SizedBox(
+                            width: 8,
+                          ),
+                          Text(
+                            'Nama',
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Expanded(
+                        child: Text(
+                      ': $fullName',
+                      style: TextStyle(fontSize: 18),
+                    )),
+                    if (isSignedIn) const Icon(Icons.edit_rounded),
+                  ],
+                ),
+                //baris tiga favorit
+                const SizedBox(height: 20),
+                Divider(color: Colors.deepPurple[100]),
+                const SizedBox(height: 4),
+                Row(
+                  children: [
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width / 3,
+                      child: const Row(
+                        children: [
+                          Icon(
+                            Icons.favorite,
+                            color: Colors.red,
+                          ),
+                          const SizedBox(
+                            width: 8,
+                          ),
+                          Text(
+                            'Favorit',
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Expanded(
+                        child: Text(
+                      ': $FavoriteCandiCount',
+                      style: TextStyle(fontSize: 18),
+                    )),
+                  ],
+                ),
+                //TODO : 4. profile action (sign in/out)
+                const SizedBox(height: 4),
+                Divider(color: Colors.deepPurple[100]),
+                const SizedBox(height: 20),
+                isSignedIn
+                    ? TextButton(
+                        onPressed: SignOut, child: const Text('Sign Out'))
+                    : TextButton(
+                        onPressed: SignIn, child: const Text('Sign In'))
+              ],
+            ),
           ),
-        ),
-      ],
-    ),
-   );
+        ],
+      ),
+    );
   }
 }
